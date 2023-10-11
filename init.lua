f005b0f5781457bc5d8086393dfcdbff9093f620
Julian Lewis <boollewis04@gmail.com>
2023-10-11 17:33:16 +1100

    First Commit

---
+++ init.lua
@@ -0,0 +1,46 @@
+-- Line Config
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Tab Config
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+
+-- Text Config
+vim.cmd[[:syntax on]]
+
+
+-- lazy.nvim Bootstrapping
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins")
+
+-- Colorscheme
+vim.cmd[[colorscheme tokyonight]]
+
+-- Snippet 
+
+ls = require("luasnip")
+-- Shift-Tab to open snippet menu
+vim.keymap.set({"i"}, "<S-Tab>", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-K>", function() ls.jump(-1) end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<C-E>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end, {silent = true})
+
+require("luasnip.loaders.from_vscode").lazy_load()
